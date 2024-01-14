0d2ee73eb65d5f12b437c856c51e21a752014ecc
Joy-Mine <1525323329@qq.com>
2024-01-15 00:04:50 +0800

    0.0.4

--- build/lib/sustech_ncs/__init__.py
+++ build/lib/sustech_ncs/__init__.py
@@ -1,6 +1,7 @@
 from .NCS_mul_pack import NCS
-from .NCS_noMul_pack import NCS as NCS_
+from .NCS_noMul_pack import NCS as NCS_noMul
+from .NCS_mul_asym import NCS as NCS_asym
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 name = "sustech_ncs"
--- sustech_ncs/__init__.py
+++ sustech_ncs/__init__.py
@@ -2,6 +2,6 @@ from .NCS_mul_pack import NCS
 from .NCS_noMul_pack import NCS as NCS_noMul
 from .NCS_mul_asym import NCS as NCS_asym
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 name = "sustech_ncs"
